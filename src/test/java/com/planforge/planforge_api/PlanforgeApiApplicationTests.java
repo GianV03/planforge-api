d1c71aa0f5a71ec539c15b61d6e6c42487e3dde9
T51574 <nttdata.ronaldo.vives@prima.com.pe>
2026-05-21 20:47:54 -0500

    first commit

---
+++ src/test/java/com/planforge/planforge_api/PlanforgeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planforge.planforge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanforgeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

68443d1ebbbf4d7959dc11c3a13f0de270e40c6b
T51574 <nttdata.ronaldo.vives@prima.com.pe>
2026-06-02 20:05:58 -0500

    Base Entity actualización

--- src/main/java/com/planforge/planforge_api/common/entities/BaseEntity.java
+++ src/main/java/com/planforge/planforge_api/common/entities/BaseEntity.java
@@ -1,8 +1,38 @@
 package com.planforge.planforge_api.common.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
 import java.time.LocalDateTime;
 
+@MappedSuperclass
 public abstract class BaseEntity {
+
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @Column(nullable = false, updatable = false)
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdated(){
+        updatedAt = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt(){
+        return updatedAt;
+    }
+
 }

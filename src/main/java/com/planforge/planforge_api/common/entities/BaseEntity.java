45a779864c8c0c7fe4f0ec7d743c59e409ac9b69
T51574 <nttdata.ronaldo.vives@prima.com.pe>
2026-06-02 19:59:38 -0500

    Created BaseEntity

---
+++ src/main/java/com/planforge/planforge_api/common/entities/BaseEntity.java
@@ -0,0 +1,8 @@
+package com.planforge.planforge_api.common.entities;
+
+import java.time.LocalDateTime;
+
+public abstract class BaseEntity {
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}

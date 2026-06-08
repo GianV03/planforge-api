8b1c420ca715f7044a2704d79b6a8b1bfc70d099
T51574 <nttdata.ronaldo.vives@prima.com.pe>
2026-06-08 15:53:42 -0500

    Creación Repositorio de ApiKey

---
+++ src/main/java/com/planforge/planforge_api/apikey/entity/ApiKeyRepository.java
@@ -0,0 +1,12 @@
+package com.planforge.planforge_api.apikey.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ApiKeyRepository extends JpaRepository<ApiKey, UUID> {
+
+    Optional<ApiKey> findByKeyValue(String keyvalue);
+
+}
